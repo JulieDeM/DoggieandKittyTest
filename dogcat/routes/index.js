9e749ee08915b4d98868f4ca0c68189ea404be60
Julie DeMasters <juliekdemasters@gmail.com>
2016-07-20 10:20:24 -0600

    added index page

--- dogcat/routes/index.js
+++ dogcat/routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'express'})
+});
+
+/* GET home page. */
 router.get('/dog', function(req, res, next) {
   res.send("Woof, I'm a dog");
 });
