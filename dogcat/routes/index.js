5679165046462fcba9bfa5dcb0732d853fce356b
Anna Lewis <anna.frances.lewis@gmail.com>
2016-07-20 10:13:25 -0600

    cat route

--- dogcat/routes/index.js
+++ dogcat/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cat', function(req, res, next) {
+  res.render('index', { title: "Meow I'm a cat" });
+});
+
 module.exports = router;
